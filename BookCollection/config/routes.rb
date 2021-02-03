f8c15b31e883e8ef84c0f1e49ed188cf8fc04eba
Maria Elisa Flores <elisa_flores98@tamu.edu>
2021-02-03 17:25:27 -0600

    linked book to home page

--- BookCollection/config/routes.rb
+++ BookCollection/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "books#index"
 
   get "/books", to: "books#index"
+  get "/books/:id", to: "books#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
