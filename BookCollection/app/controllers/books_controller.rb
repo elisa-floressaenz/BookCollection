fbb16e7ab82934b958a372ba22bee1fd4ce77929
Maria Elisa Flores <elisa_flores98@tamu.edu>
2021-02-03 18:13:55 -0600

    created and developed New view

--- BookCollection/app/controllers/books_controller.rb
+++ BookCollection/app/controllers/books_controller.rb
@@ -3,7 +3,27 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new 
+    @book = Book.new
+  end
+  
   def show
     @book = Book.find(params[:id])
   end
+
+  def create
+    @book = Book.new(title: "...", author: "...", genre: "...", price: 0, publishedDate: "...")
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :genre, :price, :publishedDate)
+    end
+
 end
--- BookCollection/config/routes.rb
+++ BookCollection/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "books#index"
 
-  get "/books", to: "books#index"
-  get "/books/:id", to: "books#show"
+  #get "/books", to: "books#index"
+  #get "/books/:id", to: "books#show"
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
